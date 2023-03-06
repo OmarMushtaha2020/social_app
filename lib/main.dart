299a91370d4384791e1555ae44c19763eb70ddb8
omarmushtaha2525gmail.com <omar200234>
2023-03-06 15:16:01 +0200

    Final Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,114 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:social/cath_helper.dart';
-import 'package:social/cubit/bloc.dart';
-import 'package:social/layout/layout_screen.dart';
-import 'package:social/login_screen.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:social/firebase_options.dart';
+import 'package:social/layout/cubit/cubit.dart';
+import 'package:social/layout/social_layout.dart';
+import 'package:social/modules/social_login/social_login_screen.dart';
+import 'package:social/shared/bloc_observer.dart';
+import 'package:social/shared/cubit/cubit.dart';
+import 'package:social/shared/cubit/states.dart';
+import 'package:social/shared/styles/themes.dart';
 
-void main()async{
-  WidgetsFlutterBinding.ensureInitialized();
+import 'package:splash_screen_view/SplashScreenView.dart';
 
-  await Firebase.initializeApp();
-  await CacthHelper.inti();
-   uid= CacthHelper.getData("uid") ;
-   print(uid);
-   Widget? widgets;
-  if(uid == null){
+import 'shared/components/componets.dart';
+import 'shared/components/constants.dart';
+import 'shared/network/local/cache_helper.dart';
 
-    widgets=LoginScreen();
-  }else{
-    widgets=LayoutScreen();
-  }
-  runApp(MyApp(widgets));
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print('on background message');
+  print(message.data.toString());
+
+  showToast(
+    text: 'on background message',
+    state: ToastStates.SUCCESS,
+  );
 }
+
+void main() async {
+  // بيتأكد ان كل حاجه هنا في الميثود خلصت و بعدين يتفح الابلكيشن
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'name-here',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  token = await FirebaseMessaging.instance.getToken();
+  print(' token is :  $token');
+  // foreground fcm
+  FirebaseMessaging.onMessage.listen((event)
+  {
+    print('on message');
+    print(event.data.toString());
+
+    showToast(text: 'on message', state: ToastStates.SUCCESS,);
+  });
+  // when click on notification to open app
+  FirebaseMessaging.onMessageOpenedApp.listen((event)
+  {
+    print('on message opened app');
+    print(event.data.toString());
+    showToast(text: 'on message opened app', state: ToastStates.SUCCESS,);
+  });
+  // background fcm
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  Bloc.observer = MyBlocObserver();
+  await CacheHelper.init();
+  bool ?isDark = CacheHelper.getData(key: 'isDark');
+  Widget? widget;
+  uId = CacheHelper.getData(key: 'uId');
+  if (uId != null) {
+    widget = SocialLayout();
+  } else {
+    widget = SocialLoginScreen();
+  }
+  runApp(MyApp(
+    isDark: isDark,
+    startWidget: widget,
+  ));
+}
+
 class MyApp extends StatelessWidget {
-  Widget? widgets;
+  final bool ?isDark;
+  final Widget? startWidget;
 
-  MyApp(this.widgets);
+  MyApp({
+    this.isDark,
+    this.startWidget,
+  });
+
   @override
-
-Widget build(BuildContext context) {
-
+  Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: [
-
-        BlocProvider(create: (context) => AppCubit()..getUserData()..getAllUser()..getALLPost()..getMyPost()),
-      ],
-      child: MaterialApp(
-        theme: ThemeData(
-scaffoldBackgroundColor: Colors.white,
-
-          appBarTheme: AppBarTheme(
-            iconTheme: IconThemeData(
-              color: Colors.black
-            ),
-            elevation: 0.0,
-            backgroundColor: Colors.white,
-
-
-          systemOverlayStyle: SystemUiOverlayStyle(
-
-            statusBarColor: Colors.white,
-          )
-          )
-        ),
-        debugShowCheckedModeBanner: false,
-home:widgets,
-      ),
-    );
+        providers: [
+          BlocProvider(
+            create: (BuildContext context) => AppCubit()
+              ..changeAppMode(
+                // fromShared: isDark!,
+              ),
+          ),
+          BlocProvider(
+            create: (BuildContext context) => SocialCubit()
+              ..getUserData()
+              ..getPosts(),
+          ),
+        ],
+        child: BlocConsumer<AppCubit, AppStates>(
+          listener: (context, state) {},
+          builder: (context, state) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              theme: lightTheme,
+              darkTheme: darkTheme,
+              themeMode: AppCubit.get(context).isDark!
+                  ? ThemeMode.dark
+                  : ThemeMode.light,
+              home:
+              startWidget,
+            );
+          },
+        ));
   }
 }
